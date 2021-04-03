685038d83cf8af7ed2d9270dbbd4a4b85c709d1f
Helgi Helgason <helgihel@gmail.com>
2021-04-03 22:39:38 +0000

    add sphere

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Box } from "./Box";
-import {Cone} from './Cone'
+import {Cone} from './Cone';
+import {Sphere} from './Sphere'
 
 const Scene = () => {
   return (
@@ -9,7 +10,9 @@ const Scene = () => {
       <ambientLight />
       <pointLight position={[16, 16, 16]} />
       <Box position={[-3.2, -4, 0]} />
-      <Cone position={[0,0,0]} />
+      <Cone position={[1,1,1]} />
+      <Sphere position={[0,0,0]} />
+
       <Box position={[2.2, 5, 0]} />
     </Canvas>
   );
---
+++ src/Sphere.jsx
@@ -0,0 +1,27 @@
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+const Sphere = (props) => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // Return view, these are regular threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <sphereGeometry args={[1, 16, 16]} />
+      <meshStandardMaterial color={hovered ? '#a62839' : '#cf6679'} />
+    </mesh>
+  )
+}
+
+export { Sphere }
